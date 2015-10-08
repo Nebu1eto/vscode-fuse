c873784a0cfd39bb770181a7d8c2d11b018e0a0c
Realignist <realignist@gmail.com>
2015-10-09 06:48:41 +0900

    Complete d.ts

--- fuse.d.ts
+++ fuse.d.ts
@@ -12,33 +12,36 @@
 
 // TODO : Work in progress...
 declare module "FuseJS/Observable" {
-    export interface Observable {
-        constructor(...object: any[]);
-        value: any;
-        length: number;
-        getAt(index: number): any;
-        add(value: any);
-        remove(value: any);
-        tryRemove(value: any);
-        removeWhere(func: (any) => any);
-        forEach(func: (any) => any);
-        replaceAt(index: number, value: any);
-        replaaceAll(array: Array<any>);
-        clear();
-        indexOf(value: any): number;
-        contains(value: any): boolean;
-        refreshAll(newValues: any, compareFunc: (oldItem: any, newItem: any) => boolean, 
-            updateFunc: (oldItem: any, newItem: any) => void, mapFunc: (newItem) => any);
-        where(condition: (object: any) => boolean): Observable;
-        map(func: (object: any) => any): Observable;
-        count(): number;
-        count(condition: (object: any) => boolean): Observable;
-        not(): boolean;
-        filter(condition: (object: any) => boolean): Observable;
-        expand(): Observable; 
+    module o {
+        interface Observable {
+            constructor(...object: any[]);
+            value: any;
+            length: number;
+            getAt(index: number): any;
+            add(value: any);
+            remove(value: any);
+            tryRemove(value: any);
+            removeWhere(func: (any) => any);
+            forEach(func: (any) => any);
+            replaceAt(index: number, value: any);
+            replaaceAll(array: Array<any>);
+            clear();
+            indexOf(value: any): number;
+            contains(value: any): boolean;
+            refreshAll(newValues: any, compareFunc: (oldItem: any, newItem: any) => boolean, 
+                updateFunc: (oldItem: any, newItem: any) => void, mapFunc: (newItem) => any);
+            where(condition: (object: any) => boolean): Observable;
+            map(func: (object: any) => any): Observable;
+            count(): number;
+            count(condition: (object: any) => boolean): Observable;
+            not(): boolean;
+            filter(condition: (object: any) => boolean): Observable;
+            expand(): Observable; 
+        }
     }
 
-    //export function constructor(...object: any[]): Observable;
+    function o (...object: any[]): o.Observable;
+    export = o;
 }
 
 /* -----------------------------------
