81de051dad349b27c9b6d03791a336cf27873b68
Realignist <realignist@gmail.com>
2015-10-09 06:03:26 +0900

    Changed Coding Styles on Fetch/Promise and implements Vibration Module.

--- fuse.d.ts
+++ fuse.d.ts
@@ -45,22 +45,22 @@ declare module "FuseJS/Observable" {
  * Define Promise
  * ---------------------------------- */
 interface Thenable<R> {
-    then<U>(onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Thenable<U>;
-    then<U>(onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => void): Thenable<U>;
+    then<U> (onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Thenable<U>;
+    then<U> (onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => void): Thenable<U>;
 }
 
 declare class Promise<R> implements Thenable<R> {
-	constructor(callback: (resolve : (value?: R | Thenable<R>) => void, reject: (error?: any) => void) => void);
-    then<U>(onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
-    then<U>(onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => void): Promise<U>;
-	catch<U>(onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
+	constructor (callback: (resolve : (value?: R | Thenable<R>) => void, reject: (error?: any) => void) => void);
+    then<U> (onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
+    then<U> (onFulfilled?: (value: R) => U | Thenable<U>, onRejected?: (error: any) => void): Promise<U>;
+	catch<U> (onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
 }
 
 declare module Promise {
-	function resolve<R>(value?: R | Thenable<R>): Promise<R>;
-	function reject(error: any): Promise<any>;
-	function all<R>(promises: (R | Thenable<R>)[]): Promise<R[]>;
-	function race<R>(promises: (R | Thenable<R>)[]): Promise<R>;
+	function resolve<R> (value?: R | Thenable<R>): Promise<R>;
+	function reject (error: any): Promise<any>;
+	function all<R> (promises: (R | Thenable<R>)[]): Promise<R[]>;
+	function race<R> (promises: (R | Thenable<R>)[]): Promise<R>;
 }
 
 /* -----------------------------------
@@ -142,7 +142,7 @@ declare type HeaderInit = Headers|Array<string>;
 declare type BodyInit = Blob|FormData|string;
 declare type RequestInfo = Request|string;
 
-declare function fetch(url: string | Request, init?: RequestInit): Promise<Response>;
+declare function fetch (url: string | Request, init?: RequestInit): Promise<Response>;
 
 /* -----------------------------------
  * Define Storage API
@@ -178,7 +178,9 @@ declare function fetch(url: string | Request, init?: RequestInit): Promise<Respo
  * require('FuseJS/Vibration');
  * ---------------------------------- */
 
-// TODO : Work in progress...
+declare module "FuseJS/Vibration" {
+	export function vibrate (time: number): void;
+}
 
 /* -----------------------------------
  * Define InterApp API
@@ -186,6 +188,6 @@ declare function fetch(url: string | Request, init?: RequestInit): Promise<Respo
  * ---------------------------------- */
 
 declare module "FuseJS/InterApp" {
-	export function launchUri(uri: string);
+	export function launchUri (uri: string): void;
 	export var onReceivedUri: (uri: string) => void; 
 }
